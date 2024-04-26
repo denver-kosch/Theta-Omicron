c2b897c869b5305921c3de56ba96546eb41d3eef
denverkosch <dkosch1@muskingum.edu>
2024-04-26 16:54:50 -0400

    initialized rush page

--- thetaomicron/src/pages/home.js
+++ thetaomicron/src/pages/home.js
@@ -3,6 +3,7 @@ import "./pagesCSS/home.css";
 import Placeholder from "../images/formalSP23.jpeg";
 
 const Home = () => {
+
     return (
         <div className="container">
             <img 
@@ -12,13 +13,14 @@ const Home = () => {
             />
             <div className="about">
                 <h2>Who are we:</h2>
-                <p>Kappa Sigma's Theta-Omicron Chapter at Muskingum University originally began as the Sphinx Club.
+                <p>
+                    Kappa Sigma's Theta-Omicron Chapter at Muskingum University originally began as the Sphinx Club.
                     <br/>In 1966, Sphinx Club was absorbed into and chartered by the International Kappa Sigma Fraternity
                     <br/>on Muskingum's campus and has continued to thrive since then.
                 </p>
             </div>
         </div>
     );
-  }
+}
 
   export default Home;
--- thetaomicron/src/pages/rush.js
+++ thetaomicron/src/pages/rush.js
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import RushGraphic from "../images/rush.jpeg";
+import "./pagesCSS/rush.css";
 
 const Rush = () => {
+    // eslint-disable-next-line
+    const [rushCom, setRush] = useState([]);
+
+    useEffect(()=>{
+        getRush();
+    } , []);
+
+    const getRush = async () => {
+        try {
+
+        } catch  (err) {
+            console.log("Error getting Rush Committee: ", err);
+        }
+     }
+
+
     return (
-      <>
-        <h1>Hello Rush!</h1>
-      </>
+      <div className="container">
+        <img
+            src={RushGraphic}
+            alt="Rush KΣ"
+            className="rushPic"
+        />
+
+        <p>Kappa Sigma is a brotherhood that prides itself on being the best of the fraternal world. At an international level, Kappa Sigma has been the best at what it does for over two decades, and we've stayed just as committed on Muskingum University's campus since out charting one decade ago. We pride ourselves on being true gentlemen who distinguish themselves by emphazing the values of the 4 pillars of Kappa Sigma: Fellowship, Leadership, Scholarship, and Service.</p>
+        
+        <br/>
+        
+        <h4>If you are interested in joining the greatest in the world, reach out to any of the brothers of the Rush Committee:</h4>
+      </div>
     );
   }
 
