c7c67da8446a7dc8b0eeafbd58c75b58a879e76a
denverkosch <dkosch1@muskingum.edu>
2024-05-20 01:10:29 -0400

    Rename styles.css to App.css and refactor

--- thetaomicron/src/App.js
+++ thetaomicron/src/App.js
@@ -9,7 +9,7 @@ import PortalHome from "./pages/portal/home";
 import {NavBar as PortalNav} from "./pages/portal/navbar";
 import Home from "./pages/mainPages/home";
 import Leadership from "./pages/mainPages/about/leadership";
-import './styles.css';
+import './App.css';
 
 
 function App() {
