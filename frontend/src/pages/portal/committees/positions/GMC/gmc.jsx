5744a517c4bc90d958b3c0a4ff4631897bbf5585
denverkosch <dkosch1@muskingum.edu>
2026-08-10 15:58:33 -0400

    refactor: streamline data fetching in GMC component and improve error handling

--- frontend/src/pages/portal/committees/positions/GMC/gmc.jsx
+++ frontend/src/pages/portal/committees/positions/GMC/gmc.jsx
@@ -4,31 +4,21 @@ import api from '@/services/apiCall';
 const GMC = () => {
     const [notepad, setNotepad] = useState("");
     const [chairReports, setReports] = useState([]);
-    const [checklist, setChecklist] = useState([]);
 
-    useEffect(() => {
-        const fetchNotepad = async () => {
-            const response = await api("getNotes", {}, {Authorization: `Bearer ${localStorage.getItem('token')}`});
-            if (response.success) setNotepad(response.notepad);
-            else console.error("Error getting notepad", response.error);
-        };
-        const fetchChairReports = async () => {
-            const response = await api("getReports", {}, {Authorization: `Bearer ${localStorage.getItem('token')}`});
-            if (response.success) setReports(response.reports);
-            else console.error("Error getting chair reports", response.error);
-        };
-        const fetchToDoList = async () => {
-            const response = await api("getChecklist", {}, {Authorization: `Bearer ${localStorage.getItem('token')}`});
-            if (response.success) setChecklist(response.checklist);
-            else console.error("Error getting checklist", response.error);
-        };
-        fetchNotepad();
-    }, []);
+    useEffect((async () => {
+        await api("getNotes", {}, {Authorization: `Bearer ${localStorage.getItem('token')}`})
+            .then(response => setNotepad(response.notepad))
+            .catch(err => console.error("Error getting notepad", err));
+        await api("getReports", {}, {Authorization: `Bearer ${localStorage.getItem('token')}`})
+            .then(response => setReports(response.reports))
+            .catch(error => console.error("Error getting chair reports", error));
+    })(), []);
     
 
     return (
         <div>
             <h1>GMC</h1>
+
         </div>
 )};
 
